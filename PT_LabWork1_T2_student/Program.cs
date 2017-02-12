ed7e3e4038dbd04c7d4e630afe52d3b9a45bb6f9
zzarkymova <zarkymova@mail.ru>
2017-02-13 01:50:28 +0600

    A code with comments

--- PT_LabWork1_T2_student/Program.cs
+++ PT_LabWork1_T2_student/Program.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace PT_LabWork1_T2_student
 {
-    public class student
+    public class student   // we create a class called student, which is accessible (public)
     {
-        public string name;
+        public string name;  // we add some fields, like name, surname, gpa
         public string surname;
         public double gpa;
-        public student()
+        public student()   // we put as a default data, which is Zeree Zarkymova with gpa 3.5
         {
-            name = "Zere";
+            name = "Zere";   
             surname = "Zarkymova";
             gpa = 3.5;
 
         }
-        public student(string a, string b, double c)
+        public student(string a, string b, double c)   
         {
             this.name = a;
             this.surname = b;
             this.gpa = c;
 
         }
-        public override string ToString()
+        public override string ToString()   // we write method ToString, which will return data as a line 
         {
             return this.name + " " + surname + " " + gpa;
         }
@@ -36,19 +36,19 @@ namespace PT_LabWork1_T2_student
     {
          static void Main(string[] args)
         {
-            student a = new student ("Altynay", "Yerkassym", 3.6);
+            student a = new student ("Altynay", "Yerkassym", 3.6);    // we create some students
             student b = new student ("Tima", "Yerikuly", 3.0);
             student c = new student ("Zhanym", "Kuatkalieva", 3.4);
             student d = new student ("Zhangul", "Demeuova", 3.4);
             student e = new student ("Aru", "Bektemirova", 3.8);
 
-            Console.WriteLine(a.ToString());
+            Console.WriteLine(a.ToString());    // we print to the console these students by using the method ToString
             Console.WriteLine(b.ToString());
             Console.WriteLine(c.ToString());
             Console.WriteLine(d.ToString());
             Console.WriteLine(e.ToString());
 
-            Console.ReadKey();
+            Console.ReadKey();   
 
         }
     }
